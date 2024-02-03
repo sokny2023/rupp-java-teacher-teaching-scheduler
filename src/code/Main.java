112329f27f4b0c13c9bf6927732e954df5ed2f28
sokny2023 <psnpsn667788@gmail.com>
2024-02-03 19:52:14 +0700

    updated menu and add method showTime()

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,10 +1,30 @@
 package code;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         View view =  new View();
         view.logo();
-        view.line();
-        view.mainMenu();
+        while (true){
+            view.mainMenu();
+            System.out.print("> Please chose option(1-7): ");
+            int option = scanner.nextInt();
+            switch (option){
+                case 1 -> System.out.println("Add Teacher");
+                case 2 -> System.out.println("View Teacher");
+                case 3 -> System.out.println("Search Teacher");
+                case 4 -> System.out.println("Update Teacher");
+                case 5 -> System.out.println("Remove Teacher");
+                case 6 -> view.showTime();
+                case 7 -> System.out.println("Show Schedule");
+                case 8 -> {
+                    System.out.println("Exit Program");
+                    System.exit(0);
+                }
+                default -> System.out.println("Invalid option, please chose again ...!");
+            }
+        }
     }
 }
--- src/code/View.java
+++ src/code/View.java
@@ -5,6 +5,7 @@ import org.nocrala.tools.texttablefmt.ShownBorders;
 import org.nocrala.tools.texttablefmt.Table;
 
 public class View {
+
     public void logo(){
         System.out.println("★".repeat(40));
         System.out.println("★".repeat(8)+" ".repeat(3)+"Royal University of Phnom Penh"+" ".repeat(3)+"★".repeat(7));
@@ -18,17 +19,33 @@ public class View {
 
     public void mainMenu(){
         Table table = new Table(1, BorderStyle.UNICODE_BOX_DOUBLE_BORDER_WIDE, ShownBorders.SURROUND);
-        Table table1 = new Table(1, BorderStyle.UNICODE_BOX_DOUBLE_BORDER_WIDE, ShownBorders.SURROUND);
-        table.setColumnWidth(0,25,25);
-        table1.addCell("  >> Main Menu << ");
-        table1.addCell("–".repeat(20));
-        table1.addCell("  1.Schedule");
-        table1.addCell("  2.Add Teacher");
-        table1.addCell("  3.Search Teacher");
-        table1.addCell("  4.Update Teacher");
-        table1.addCell("  5.Remove Teacher");
-        table1.addCell("  6.Setting");
-        table1.addCell("  7.Exit Program");
+        table.setColumnWidth(0,20,25);
+        table.addCell("  >> Main Menu << ");
+        table.addCell("–".repeat(20));
+        table.addCell("  1.Add Teacher");
+        table.addCell("  2.View Schedule");
+        table.addCell("  3.Search Teacher");
+        table.addCell("  4.Update Teacher");
+        table.addCell("  5.Remove Teacher");
+        table.addCell("  6.Show Time");
+        table.addCell("  7.Show Schedule");
+        table.addCell("  8.Exit Program");
+        System.out.println(table.render());
+    }
+
+    public void showTime(){
+        System.out.println(" ".repeat(15)+"⏰ Show Time ⏰");
+        Table table = new Table(2, BorderStyle.UNICODE_BOX_DOUBLE_BORDER_WIDE, ShownBorders.ALL);
+        table.setColumnWidth(0,20,25);
+        table.setColumnWidth(0,20,25);
+        table.addCell(" ".repeat(5)+" Shift ");
+        table.addCell(" ".repeat(5)+" Time ");
+        table.addCell(" 1. Morning ");
+        table.addCell(" 7:30 -> 10:45 AM ");
+        table.addCell(" 2. Afternoon ");
+        table.addCell(" 2:15 -> 5:15 PM ");
+        table.addCell(" 3. Evening ");
+        table.addCell(" 5:30 -> 8:45 PM ");
         System.out.println(table.render());
     }
 }
